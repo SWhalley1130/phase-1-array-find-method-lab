ae50c5281c1afe8fc61468fa2490a9be7c231081
Sarah Whalley <s.whalley1130@gmail.com>
2023-02-05 16:48:39 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record)
+{
+    const firstWinningYear=record.find((e)=> e.result==="W")
+    if (!firstWinningYear)
+    {
+        return undefined;
+    }
+    else 
+    {
+        return firstWinningYear.year;
+    }
+}
